d35c739f4b10b3470c57e6ace454281b814ee3da
chad <chad@chads-MacBook-Air.local>
2019-04-05 13:12:33 -0500

    fix

--- kubeup-nodes-install-ubuntu.sh
+++ kubeup-nodes-install-ubuntu.sh
@@ -1,1 +1,5 @@
+sudo apt update -y
+
+sleep 2m
+
 sudo apt install -y docker-ce=18.06.2~ce~3-0~ubuntu kubelet kubeadm kubectl
--- kubeup-nodes-repoadd-ubuntu.sh
+++ kubeup-nodes-repoadd-ubuntu.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
