eb4122744675300e108a79c3b66f8788ceabac7a
chad <chad@chads-MacBook-Air.local>
2019-04-05 12:25:03 -0500

    kube up on ubuntu for all k8s nodes

--- kubeup-nodes-ubuntu.sh
+++ kubeup-nodes-ubuntu.sh
@@ -1,1 +1,13 @@
-docker-ce=18.06.2~ce~3-0~ubuntu
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list 
+deb https://apt.kubernetes.io/ kubernetes-xenial main 
+EOF
+
+sudo apt install -y docker-ce=18.06.2~ce~3-0~ubuntu
+
+sudo apt install -y kubelet kubeadm kubectl
