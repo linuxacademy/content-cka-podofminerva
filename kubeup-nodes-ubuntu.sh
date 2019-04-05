2ad6c1dbca36d2129407e324425dffdf3e89ac25
chad <chad@chads-MacBook-Air.local>
2019-04-05 12:44:34 -0500

    kube up on nodes kubernetes

--- kubeup-nodes-ubuntu.sh
+++ kubeup-nodes-ubuntu.sh
@@ -4,8 +4,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list 
-deb https://apt.kubernetes.io/ kubernetes-xenial main 
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
-sudo apt install -y docker-ce=18.06.2~ce~3-0~ubuntu kubelet kubeadm kubectl
