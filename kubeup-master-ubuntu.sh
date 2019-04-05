531ab178e9537b5a46b49b2335df3f28cad34565
chad <chad@chads-MacBook-Air.local>
2019-04-05 14:18:05 -0500

    kube up on ubuntu

--- kubeup-master-ubuntu.sh
+++ kubeup-master-ubuntu.sh
@@ -1,3 +1,5 @@
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+sleep 90
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
